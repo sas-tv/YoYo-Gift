a79ef2cde4775cd86d470f62126f41dfb1763bce
SAS TV <knowledge.com2k16@gmail.com>
2022-01-22 01:48:56 +0530

    updated category dropdown text

--- next.config.js
+++ next.config.js
@@ -5,5 +5,6 @@ module.exports = {
     DB_PASSWORD: 'redeem-a-gift-bro',
     SECRET: "269f7c2565636160fea419b16fc0da9a",
     APP_URL: "https://yo-yo-gift.vercel.app/"
+    // APP_URL: "http://localhost:3000"
   }
 }
--- src/components/GiftCardsFilter/CategoryFilter/index.jsx
+++ src/components/GiftCardsFilter/CategoryFilter/index.jsx
@@ -11,7 +11,7 @@ const categoryFilterData = [
   'Ecommerce',
   'Entertainment',
   'Travel',
-  'Health',
+  'Beauty',
   'Food'
 ];
 
