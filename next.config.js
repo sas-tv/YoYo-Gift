ff1c5e2f53706ff5ab7406c37be125ba16ff870e
SAS TV <knowledge.com2k16@gmail.com>
2022-01-21 23:30:21 +0530

    updated config app url

--- next.config.js
+++ next.config.js
@@ -4,6 +4,6 @@ module.exports = {
     DB_USERNAME: 'application-user',
     DB_PASSWORD: 'redeem-a-gift-bro',
     SECRET: "269f7c2565636160fea419b16fc0da9a",
-    APP_URL: "http://localhost:3000/"
+    APP_URL: "https://yo-yo-gift.vercel.app/"
   }
 }
